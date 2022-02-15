2c391313877b048f3bef9bc69232eeae4d21e9ca
Jean Carlo EM <jeancarlo@jeancarloem.com>
2022-02-14 22:49:44 -0300

    corração do .alternativenames, modelo.conf e do criardominio.sh

--- src/createdomain.sh
+++ src/createdomain.sh
@@ -21,46 +21,65 @@ if [[ ( -z "$_DOMINIO" ) || ( -z "$_USER" ) || ( ! $_DOMINIO =~ $REGEXDOM ) && (
 	exit 1
 fi
 
-#
-# CRIA A PASTA DO DOMINIO
-#
-if [[ ! -d "$_dom_path_/$_DOMINIO/" ]];
-    mkdir -p "$_dom_path_/$_DOMINIO/"
-fi
-
-__RWXAcess "$_dom_path_/$_DOMINIO" "rx" "$_USER"
+local dpath="$_dom_path_/$_DOMINIO"
 
 # CRIA OS SUBDIRETORIOS
-mkdir -p "$_dom_path_/$_DOMINIO/.subs/www/"
-mkdir -p "$_dom_path_/$_DOMINIO/.subs/@/"
-mkdir -p "$_dom_path_/$_DOMINIO/.tls/"
+for sd in "/" "/.subs/www/" "/.subs/@/" "/.tls/". do
+	if [[ ! -d "${dpath}$sd" ]]; then
+		mkdir -p "${dpath}$sd"
+	fi
+done
 
-# AS CONFIIGURACOES DO USUARIO
-ln -sf /home/$_USER/.webconfig $_dom_path_/$_DOMINIO/.webconfig
-yes | rm -f /home/$_USER/.webconfig/.webconfig
-
-# DEFINE O ACESSO PADRAO PARA O OS SUBDOMINIOS
-__RWXAcess "$_dom_path_/$_DOMINIO/.subs" "rwx" "$_USER"
-
-# CRIANDO CONFIGURACAO OPCIONAL NGINX
-touch $_dom_path_/$_DOMINIO/.nginx.conf
-
-# A PAGINA INDEX DEFAULT
-if [[ ! -f "$_dom_path_/$_DOMINIO/.subs/www/index.html" ]]; then
-	yes | /bin/cp -rf /etc/nginx/template/index.html "$_dom_path_/$_DOMINIO/.subs/www/"
+# CRIA O ARQUIVO DE SUBDOMINIOS
+if [[ ! -f "$dpath/.alternativenames" ]]; then
+	touch "$dpath/.alternativenames"
 fi
 
-if [[ ! -f "$_dom_path_/$_DOMINIO/.subs/@/index.html" ]]; then
-	yes | /bin/cp -rf /etc/nginx/template/index.html "$_dom_path_/$_DOMINIO/.subs/@/"
+# CRIA AQUIVOS DE LOG E ERRO
+if [[ ! -f "/var/log/nginx/$_DOMINIO/error.log" ]]; then
+	touch "/var/log/nginx/$_DOMINIO/error.log"
+fi
+
+if [[ ! -f "/var/log/nginx/$_DOMINIO/error.log" ]]; then
+	touch "/var/log/nginx/$_DOMINIO/access.log"
+fi
+
+# CRIA LINK SIMBOLISCOS PARA OS AQUIVOS DE LOG E ERROS
+if [[ ! -f "$dpath/.nginx.error.log" ]]; then
+	ln -s "/var/log/nginx/$_DOMINIO/error.log" "$dpath/.nginx.error.log" 
+fi
+
+if [[ ! -f "$dpath/.nginx.access.log" ]]; then
+	ln -s "/var/log/nginx/$_DOMINIO/access.log" "$dpath/.nginx.access.log" 
+fi
+
+# AS CONFIIGURACOES DO USUARIO
+ln -sf /home/$_USER/.webconfig $dpath/.webconfig
+yes | rm -f /home/$_USER/.webconfig/.webconfig
+
+# CRIANDO CONFIGURACAO OPCIONAL NGINX
+if [[ ! -f "$dpath/.nginx.conf" ]]; then
+	touch $dpath/.nginx.conf
+fi
+
+# CONFIGURA A PAGINA INDEX DEFAULT www
+if [[ ! -f "$dpath/.subs/www/index.html" ]]; then
+	yes | /bin/cp -rf /etc/nginx/template/index.html "$dpath/.subs/www/"
+fi
+
+# CONFIGURA A PAGINA INDEX DEFAULT @
+if [[ ! -f "$dpath/.subs/@/index.html" ]]; then
+	yes | /bin/cp -rf /etc/nginx/template/index.html "$dpath/.subs/@/"
 fi
 
 # CRIAR A CONFIGURAÇÃO DO DOMINIO
 if [[ ! -f /etc/nginx/conf.d/$_USER/$_DOMINIO.conf ]]; then
-	mkdir -p /etc/nginx/conf.d/$_USER/
-
-	local dpath="$_dom_path_/$_DOMINIO"
-	local tlspath="$_dom_path_/$_DOMINIO/.tls"
-	local dominio_excap=$(echo "jeancarloem.com" | sed "s/\\./\\\./g")
+	if [[ ! -d "/etc/nginx/conf.d/$_USER/" ]]; then
+		mkdir -p /etc/nginx/conf.d/$_USER/
+	fi
+	
+	local tlspath="$dpath/.tls"
+	local dominio_excap=$(echo "$_DOMINIO" | sed "s/\\./\\\./g")
 fi
 
 cat /etc/nginx/template/MODELO.conf | sed "s/##{DOMINIO}/$_DOMINIO_excap/g" >> /etc/nginx/conf.d/$_USER/$_DOMINIO.conf	
